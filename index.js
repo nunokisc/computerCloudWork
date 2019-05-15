4311ac939094967cf599fa56f493b86b2295758f
Nuno Cardoso <nuno.cardoso@tagusvalley.pt>
2019-05-15 14:40:03 +0100

    add php-fpm, add loadbalancer, change php-fpm to custom docker image with php-mysqli, change from mysql-server to mariadb, bind conf.d folders, bind html folder in nginx and php-fpm container

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ var Docker = require('dockerode');
 var docker = new Docker({socketPath: '/var/run/docker.sock'});
 var containersIDs = [];
 
-//RUN SYSTEM CONTAINERS IF THEY'RE NOT RUNNING
-docker.createContainer({Image: 'nginx', Cmd: ['nginx', '-g', 'daemon off;'], name: 'nginx-sv1'}, function (err, container) {
+//RUN SYSTEM CONTAINERS IF THEY'RE NOT RUNNING ---------------------------------------------------------------------------------------------------------------------------
+
+docker.createContainer({Image: 'nginx', Cmd: ['nginx', '-g', 'daemon off;'], name: 'nginx-sv1', HostConfig: {'Binds': ['/home/nuno/computerCloudWork/html:/var/www/html','/home/nuno/computerCloudWork/http/conf.d:/etc/nginx/conf.d']}}, function (err, container) {
 	container.start(function (err, data) {
 		console.log(data);
 	});
@@ -13,12 +14,41 @@ docker.createContainer({Image: 'nginx', Cmd: ['nginx', '-g', 'daemon off;'], nam
 			getContainerDataRunning(data.Config.Hostname);
 		},3000);
 	});
-	container.putArchive('index.tar', {path:'/usr/share/nginx/html'});
-	//container.remove();
 
+	// to put archive in container
+	//container.putArchive('index.tar', {path:'/usr/share/nginx/html'});
+	//container.remove();
 });
 
-docker.createContainer({Image: 'mysql', Cmd: ['mysqld'], name: 'mysql-server', Env:['MYSQL_ROOT_PASSWORD=root','MYSQL_DATABASE=teste_grafana'], HostConfig: {'Binds': ['/home/nuno/computerCloudWork/db:/docker-entrypoint-initdb.d/']}}, function (err, container) {
+docker.createContainer({Image: 'nginx', Cmd: ['nginx', '-g', 'daemon off;'], name: 'nginx-lb1', HostConfig: {'Binds': ['/home/nuno/computerCloudWork/loadbalancer/conf.d:/etc/nginx/conf.d']}}, function (err, container) {
+	container.start(function (err, data) {
+		console.log(data);
+	});
+	container.inspect(function (err, data) {
+		containersIDs.push(data.Config.Hostname);
+		setTimeout(function(){
+			getContainerDataRunning(data.Config.Hostname);
+		},3000);
+	});
+
+	// to put archive in container
+	//container.putArchive('index.tar', {path:'/usr/share/nginx/html'});
+	//container.remove();
+});
+
+docker.createContainer({Image: 'kisc/php-fpm-kisc', Cmd: [], name: 'php-fpm-sv1', HostConfig: {'Binds': ['/home/nuno/computerCloudWork/html:/var/www/html']}}, function (err, container) {
+	container.start(function (err, data) {
+		console.log(data);
+	});
+	container.inspect(function (err, data) {
+		containersIDs.push(data.Config.Hostname);
+		setTimeout(function(){
+			getContainerDataRunning(data.Config.Hostname);
+		},3000);
+	});
+});
+
+docker.createContainer({Image: 'mariadb', Cmd: ['mysqld'], name: 'mysql-server', Env:['MYSQL_ROOT_PASSWORD=root','MYSQL_DATABASE=wordpress'], HostConfig: {'Binds': ['/home/nuno/computerCloudWork/db:/docker-entrypoint-initdb.d/']}}, function (err, container) {
 
 	var options = {
 		Cmd: ['bash', '-c', 'echo "bind-address = 0.0.0.0" >> /etc/mysql/my.cnf'],
@@ -52,17 +82,15 @@ docker.createContainer({Image: 'mysql', Cmd: ['mysqld'], name: 'mysql-server', E
 
 });
 
-docker.listContainers(function (err, containers) {
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    containersIDs.push(containers.Config.Hostname);
-
-});
 
 function getContainerDataRunning(id)
 {
 	let container = docker.getContainer(id);
 	container.inspect(function (err, data) {
-		console.log(data);
+		console.log(data.Name)
+		console.log(data.NetworkSettings.IPAddress);
 	});
 
 }
