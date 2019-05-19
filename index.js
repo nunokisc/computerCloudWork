7a4da3251479d7252bfb3b609d7190b91c650617
kisc <nuno_kisc@hotmail.com>
2019-05-19 01:25:55 +0100

    add socket.io event getOnlineContainers

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const influx = new Influx.InfluxDB({
 var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 var path = require('path');
 var NginxConfFile = require('nginx-conf').NginxConfFile;
 const fs = require('fs')
@@ -464,6 +465,16 @@ app.use(express.static(htmlPath));
 
 http.listen(3000, function(){
 	console.log('listening on *:3000');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('getOnlineContainers',function(){
+  	socket.emit('getOnlineContainers',onlineContainers);
+  })
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
 
 /*setTimeout(()=>{
